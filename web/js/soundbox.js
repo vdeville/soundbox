b3ccfa6049619dd990812b567f95b3b680fecf4d
Valentin Deville <contact@valentin-deville.eu>
2018-05-21 22:52:50 +0200

    Update library and custom fix while author of this lib don't accetp my PR

--- web/js/soundbox.js
+++ web/js/soundbox.js
@@ -10,7 +10,10 @@
 
 class SoundBox {
 	constructor() {
-		this.sounds = {};
+		this.sounds = {}; // The loaded sounds and their instances
+		this.instances = []; // Sounds that are currently playing
+		
+		this.default_volume = 1;
 	}
 	
 	load(sound_name, path, callback) {
@@ -27,30 +30,41 @@ class SoundBox {
 	remove(sound_name) {
 		if(typeof this.sounds != "undefined")
 			delete this.sounds[sound_name];
-		if(typeof this.sound_callbacks == "function")
-			delete this.sound_callbacks[sound_name];
 	};
 	
-	play(sound_name, callback, volume = 1) {
+	play(sound_name, callback, volume = null) {
 		if(typeof this.sounds[sound_name] == "undefined") {
 			console.error("Can't find sound called '" + sound_name + "'.");
 			return false;
 		}
 		
 		var soundInstance = this.sounds[sound_name].cloneNode(true);
-		soundInstance.volume = volume;
+		soundInstance.volume = volume || this.default_volume;
 		soundInstance.play();
+		this.instances.push(soundInstance);
 		
+		// Don't forget to remove the instance from the instances array
+		soundInstance.addEventListener("ended", () => {
+			let index = this.instances.indexOf(soundInstance, 1);
+			if(index != -1) this.instances.splice(index, 1);
+		});
+		
+		// Attach the callback / promise
 		if(typeof callback == "function") {
 			soundInstance.addEventListener("ended", callback);
 			return true;
 		}
-		else {
-			return new Promise((resolve, reject) => soundInstance.addEventListener("ended", resolve));
-		}
+		return new Promise((resolve, reject) => soundInstance.addEventListener("ended", resolve));
 	};
+	
+	stop_all() {
+		// Pause all currently playing sounds
+		for (let instance of this.instances) {
+            instance.dispatchEvent((new Event('ended')));
+            instance.pause();
+        }
+		this.instances = []; // Empty the instances array
+	}
 }
 
-SoundBox.version = "0.3";
-
-export default SoundBox;
+SoundBox.version = "0.3.2";
